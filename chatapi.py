270fc51589f92ae60355679321f0ec149a689443
Rishi Mantri <rishimantri795@gmail.com>
2023-10-31 12:01:18 -0400

    chatapi working

--- chatapi.py
+++ chatapi.py
@@ -1,8 +1,20 @@
 import requests
 
-files = [
-    ('file', ('file', open('PHYS172_F23_Syllabus&Schedule_2023-10-05.pdf','rb'), 'application/octet-stream'))
-]
+course = input("What course are you interested in? ")
+
+if course == "PHYS 172":
+    files = [
+    ('file', ('file', open('PHYS172_F23_Syllabus&Schedule_2023-10-05.pdf','rb'), 'application/octet-stream'))]
+elif course == "MA 261":
+    files = [
+    ('file', ('file', open('syllabus_ma261_fa23.pdf','rb'), 'application/octet-stream'))]
+elif course == "CS 159":
+    files = [
+    ('file', ('file', open('syllabus.pdf','rb'), 'application/octet-stream'))]
+elif course == "ENGR 133":
+    files = [
+    ('file', ('file', open('ENGR 133_Fa23_Syllabus_V4.pdf','rb'), 'application/octet-stream'))]
+
 headers = {
     'x-api-key': 'sec_AZ2dsLvF11l5eFMuq3ICSpkkREDPiJVB'
 }
@@ -16,19 +28,20 @@ else:
     print('Status:', response.status_code)
     print('Error:', response.text)
 
-
-
 headers = {
     'x-api-key': 'sec_AZ2dsLvF11l5eFMuq3ICSpkkREDPiJVB',
     "Content-Type": "application/json",
 }
+
+question = input("What's your question?")
+print(question)
 
 data = {
     'sourceId': response.json()['sourceId'],
     'messages': [
         {
             'role': "user",
-            'content': "Where are office hours for the professor?",
+            'content': question,
         }
     ]
 }
