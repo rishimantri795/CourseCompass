3668d27666686dbe95e81a817f35ea344a0abe1e
Rishi Mantri <rishimantri795@gmail.com>
2023-12-01 12:12:50 -0500

    teacher specific grade distributions

--- test.py
+++ test.py
@@ -20,10 +20,7 @@ class GUIApp:
         self.button = tk.Button(master, text="Submit", command=self.show_outputs)
         self.button.pack()
 
-        self.output_listbox = tk.Listbox(master)
-        self.output_listbox.pack()
-
-        self.label_dropdown = tk.Label(master, text="Choose from the list:")
+        self.label_dropdown = tk.Label(master, text="Choose from the list (will populate on submission):")
         self.label_dropdown.pack()
 
         # Create a StringVar to store the selected option
@@ -33,9 +30,20 @@ class GUIApp:
         self.dropdown = ttk.Combobox(master, textvariable=self.selected_option, state="readonly")
         self.dropdown.pack()
 
+        label = tk.Label(text="Course Grade Distribution:")
+        label.pack()
+
+        self.output_listbox = tk.Listbox(master)
+        self.output_listbox.pack()
+
+        
+
     def show_outputs(self):
         # Get class number from the entry widget
+
         class_number = self.entry.get()
+
+        instructor = self.selected_option.get()
 
         # Validate input
         if not class_number.isdigit():
@@ -47,14 +55,14 @@ class GUIApp:
         self.dropdown['values'] = ()
 
         # Generate and display list of outputs
-        outputs = self.generate_outputs(int(class_number))
+        outputs = self.generate_outputs(int(class_number), instructor)
         for output in outputs:
             self.output_listbox.insert(tk.END, output)
 
         # Populate drop-down list with options
         self.dropdown['values'] = tuple(self.generate_dropdown_options(class_number))
 
-    def generate_outputs(self, class_number):
+    def generate_outputs(self, class_number, instructor):
 
         def read_csv_data(class_number):
             i = 0
@@ -77,41 +85,68 @@ class GUIApp:
             df = pd.DataFrame(data)
             df.to_csv('classdata.csv', index=False)
                     
-                    
             return i-1,start,data
-
+                  
         end,begin,mydata = read_csv_data(class_number)
-        #print(f"The row numbers occupied by class number {class_number} is: {begin} to {end}")
-        grades = np.zeros((16,end-begin+1))
 
-        index = -1
+        all_instructors = []
+        gradeLevels = []
+        dict_instructor_grades = {}
         with open('classdata.csv', 'r') as file:
-            reader = csv.reader(file)
-            for row in reader:
-                
+            reader3 = csv.reader(file)
+            all_instructors.append('All')
+            for row in reader3:
+                gradeLevels = []
                 if row[1] == '1':
                     pass
                 else:
-                    index +=1
-                    for i in range(8,24):
-                        if row[i] == '':
-                            value = 0
-                        else:
-                            value = float(re.search(r'\d+\.\d+',row[i]).group())
+                    if row[7] not in all_instructors:
+                        all_instructors.append(row[7])
+                        for i in range(8,24):
+                            if row[i] == '':
+                                value = 0
+                            else:
+                                value = float(re.search(r'\d+\.\d+',row[i]).group())
+                            gradeLevels.append(value)
+                        dict_instructor_grades.update({row[7]:gradeLevels})  
+            print(dict_instructor_grades)
 
-                        grades[i-8][index] = value
+        if instructor not in all_instructors:
+            instructor = 'All'
 
-                        #grades[i-8][0] = float(row[i].strip('.%'))                    
-        averages = np.zeros(16)
-        for i in range(0,16):
-            averages[i] = round(np.mean(grades[i]),2)
+        if instructor == 'All' or instructor == '':
+            grades = np.zeros((16,end-begin+1))
+            index = -1
+            with open('classdata.csv', 'r') as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    
+                    if row[1] == '1':
+                        pass
+                    else:
+                        index +=1
+                        for i in range(8,24):
+                            if row[i] == '':
+                                value = 0
+                            else:
+                                value = float(re.search(r'\d+\.\d+',row[i]).group())
+
+                            grades[i-8][index] = value
+
+            averages = np.zeros(16)
+            for i in range(0,16):
+                averages[i] = round(np.mean(grades[i]),2)
+            
+            dict = {'A': averages[0], 'A-': averages[1], 'A+': averages[2], 'AU': averages[3], 'B': averages[4], 'B-': averages[5], 'B+': averages[6], 'C': averages[7], 'C-': averages[8], 'C+': averages[9], 'D': averages[10], 'D-': averages[11], 'D+': averages[12], 'E': averages[13], 'F': averages[14], 'I': averages[15]}
+            
+            return [f"Grade {i} : {dict[i]}%" for i in dict]
         
-        dict = {'A': averages[0], 'A-': averages[1], 'A+': averages[2], 'AU': averages[3], 'B': averages[4], 'B-': averages[5], 'B+': averages[6], 'C': averages[7], 'C-': averages[8], 'C+': averages[9], 'D': averages[10], 'D-': averages[11], 'D+': averages[12], 'E': averages[13], 'F': averages[14], 'I': averages[15]}
-
-        for i in dict:
-            print(f"The average grade for {i} is {dict[i]}%")
-        
-        return [f"Grade {i} : {dict[i]}%" for i in dict]
+        else:
+            grades = dict_instructor_grades[instructor]
+            averages = grades
+            dict = {'A': averages[0], 'A-': averages[1], 'A+': averages[2], 'AU': averages[3], 'B': averages[4], 'B-': averages[5], 'B+': averages[6], 'C': averages[7], 'C-': averages[8], 'C+': averages[9], 'D': averages[10], 'D-': averages[11], 'D+': averages[12], 'E': averages[13], 'F': averages[14], 'I': averages[15]}
+            
+            return [f"Grade {i} : {dict[i]}%" for i in dict]
 
     def generate_dropdown_options(self,class_number):
 
@@ -119,6 +154,7 @@ class GUIApp:
 
         with open('classdata.csv', 'r') as file:
             reader = csv.reader(file)
+            instructors.append('All')
             for row in reader:
                 if row[1] == '1':
                     pass
